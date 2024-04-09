818196881ea094a7bf77d4d0d18b4a12a0de7391
jiang <emailtojiang@gmail.com>
2024-04-09 17:14:40 +0800

    配置文件更新

--- src/assets/js/default-data.js
+++ src/assets/js/default-data.js
@@ -1,7 +1,18 @@
-import {LowercaseLetters, Numbers, SpecialCharacters, UppercaseLetters} from "./base-data.js";
+import {LengthTypeKeys, LowercaseLetters, Numbers, SpecialCharacters, UppercaseLetters} from "./base-data.js";
 
-// 默认配置
-export const defaultConfig = {
+const defaultConfigV2 = {
+    fixedLength: 16,
+    rangeLength: [8, 32],
+    lengthType: LengthTypeKeys.Fixed,
+    maxLength: 64,
+    minLength: 1,
+    enabledNumbers: Numbers.split('').map((char) => ({key: char, label: char, checked: true})),
+    enabledLowercaseLetters: LowercaseLetters.split('').map((char) => ({key: char, label: char, checked: true})),
+    enabledUppercaseLetters: UppercaseLetters.split('').map((char) => ({key: char, label: char, checked: true})),
+    enabledSpecialCharacters: SpecialCharacters.split('').map((char) => ({key: char, label: char, checked: true})),
+}
+
+const defaultConfigV1 = {
     length: 16,
     maxLength: 64,
     minLength: 1,
@@ -10,3 +21,6 @@ export const defaultConfig = {
     enabledUppercaseLetters: UppercaseLetters.split('').map((char) => ({key: char, label: char, checked: true})),
     enabledSpecialCharacters: SpecialCharacters.split('').map((char) => ({key: char, label: char, checked: true})),
 }
+
+// 默认配置
+export const defaultConfig = defaultConfigV2;
